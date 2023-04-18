539d8e397bce4b30273ef190d81323bc192e3233
Omar <mm5094397@gmail.com>
2023-04-18 13:18:08 +0200

    custom appbar

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
